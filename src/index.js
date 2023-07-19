6c6705e5486699339c389cc5d5cd3ea35b7f5160
Bogdan Nazarchuk <111483751+Virtuozio@users.noreply.github.com>
2023-07-20 00:28:53 +0300

    some updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter basename="/YourPet">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );

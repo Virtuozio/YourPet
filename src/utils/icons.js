3f789a7a29c830c35e151e4c47577aa44447bd8b
Bogdan Nazarchuk <111483751+Virtuozio@users.noreply.github.com>
2023-07-22 12:15:42 +0300

    repair

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -3,5 +3,5 @@ import { ReactComponent as SearchIcon } from 'assets/images/icons/search.svg';
 import { ReactComponent as FilterIcon } from 'assets/images/icons/filters-3.svg';
 import { ReactComponent as PlusIcon } from 'assets/images/icons/plus.svg';
 import { ReactComponent as PlusSmallIcon } from 'assets/images/icons/plus-small.svg';
-import { ReactComponent as Pawprint } from 'assets/images/icons/pawprint 1.svg';
+import { ReactComponent as Pawprint } from 'assets/images/icons/pawprint.svg';
 export { CrossIcon, SearchIcon, FilterIcon, PlusIcon, PlusSmallIcon, Pawprint };

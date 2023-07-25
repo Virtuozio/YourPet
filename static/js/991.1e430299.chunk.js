65940e268be4fa26548f5d0f17a794da4554880e
Virtuozio <111483751+Virtuozio@users.noreply.github.com>
2023-07-25 15:38:08 +0000

    Deploying to gh-pages from @ Virtuozio/YourPet@32d73f097fcac2f2864b428d9d670d285f9bb033 🚀

--- static/js/991.1e430299.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyour_pet=self.webpackChunkyour_pet||[]).push([[991],{8991:function(n,e,r){r.r(e),r.d(e,{default:function(){return c}});r(2791);var t=r(184),u=function(){return(0,t.jsx)("div",{children:"AddPetForm"})},c=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(u,{})})}}}]);
-//# sourceMappingURL=991.1e430299.chunk.js.map

77becf0f484e57d55fc4e76dc581d907efb0f34c
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-07-28 05:11:16 +0300

    pagination

--- src/pages/Notices/Notices.jsx
+++ src/pages/Notices/Notices.jsx
@@ -5,6 +5,8 @@ import NoticesFilters from 'components/NoticesFilters/NoticesFilters';
 import NoticesSearch from 'components/NoticesSearch/NoticesSearch';
 import React from 'react';
 
+
+
 // import ModalUnauthorized from 'components/ModalUnauthorized/ModalUnauthorized';
 // import ModalDeleteAction from 'components/ModalDeleteAction/ModalDeleteAction';
 // import Backdrop from 'components/Backdrop/Backdrop';
@@ -12,11 +14,16 @@ import React from 'react';
 import { Title, Wrapper, Container, Filters } from './Notices.styled';
 // import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   fetchFavoriteNotices,
   fetchNotices,
 } from 'redux/notices/noticesOperations';
+
+import Pagination from '@mui/material/Pagination';
+
+// import { makeStyles } from "@material-ui/core/styles";
+
 // import ModalNotice from 'components/ModalNotice/ModalNotice';
 const Notices = () => {
   // const [isModalOpen, setIsModalOpen] = useState(true); //поміняти значення на false*true//
@@ -24,12 +31,20 @@ const Notices = () => {
   // const closeModal = () => {
   //   setIsModalOpen(prevState => !prevState);
   // };
+  // const pageSize = 4;
+  const [page, setPage] = useState(1);
+
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchNotices());
+    dispatch(fetchNotices(`?page=${page}&limit=8` ));
     dispatch(fetchFavoriteNotices());
-  }, [dispatch]);
+  }, [dispatch, page]);
+
+   const handleChange = (e, p) => {
+
+      setPage(p)
+    }
 
   return (
     <>
@@ -55,6 +70,26 @@ const Notices = () => {
           </Container>
         </Filters>
         <NoticesCategoriesList />
+       
+        <Pagination
+          count={2}
+          size="large"
+          variant="outlined"
+          color="primary"
+       
+            showFirstButton
+            showLastButton
+            // siblingCount={1}
+            // boundaryCount={0}
+            onChange={handleChange}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginBottom: '100px',
+              
+            }} />
+         
         {/* <ModalNotice /> */}
       </Wrapper>
     </>

e53e1234d55445731efa274eec4edfef9475163c
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-07-29 17:58:57 +0300

    add Time modal

--- src/components/OurFriendsInfo/OurFriendsInfo.jsx
+++ src/components/OurFriendsInfo/OurFriendsInfo.jsx
@@ -1,37 +1,141 @@
 import React from 'react';
-import { ContentBox } from './OurFriendsInfo.styled';
+import { useState } from 'react';
+import { ContentBox, InfoBox, ModalContent } from './OurFriendsInfo.styled';
 
-const OurFriendsInfo = ({ address, email, phone }) => {
+const OurFriendsInfo = ({ workDays, address, email, phone }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(prevIsModalOpen => !prevIsModalOpen);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleClickOutside = e => {
+    if (e.target.classList.contains('modal-backdrop')) {
+      closeModal();
+    }
+  };
+
+  // if (!workDays || workDays.length === 0) {
+  //   return (
+  //     <ContentBox>
+  //       <p>Дані відсутні</p>
+  //     </ContentBox>
+  //   );
+  // }
+
   return (
     <>
       <ContentBox>
         <ul>
           <li>
-            <a href="">
-              <b>Time</b>
-              <p>{}</p>
-            </a>
+            <InfoBox>
+              <b onClick={toggleModal}>Time</b>
+              {isModalOpen && (
+                <div className="modal">
+                  <ModalContent>
+                    {!workDays || workDays.length === 0 ? (
+                      <p>Дані відсутні</p>
+                    ) : (
+                      <ul>
+                        <li>
+                          <b>Пн:</b>
+                          <p>
+                            {workDays[0].isOpen
+                              ? `${workDays[0].from} - ${workDays[0].to}`
+                              : 'Зачинено'}
+                          </p>
+                        </li>
+                        <li>
+                          <b>Вт:</b>
+                          <p>
+                            {workDays[1].isOpen
+                              ? `${workDays[1].from} - ${workDays[1].to}`
+                              : 'Зачинено'}
+                          </p>
+                        </li>
+                        <li>
+                          <b>Ср:</b>
+                          <p>
+                            {workDays[2].isOpen
+                              ? `${workDays[2].from} - ${workDays[2].to}`
+                              : 'Зачинено'}
+                          </p>
+                        </li>
+                        <li>
+                          <b>Чт:</b>
+                          <p>
+                            {workDays[3].isOpen
+                              ? `${workDays[3].from} - ${workDays[3].to}`
+                              : 'Зачинено'}
+                          </p>
+                        </li>
+                        <li>
+                          <b>Пт:</b>
+                          <p>
+                            {workDays[4].isOpen
+                              ? `${workDays[4].from} - ${workDays[4].to}`
+                              : 'Зачинено'}
+                          </p>
+                        </li>
+                        <li>
+                          <b>Сб:</b>
+                          <p>
+                            {workDays[5].isOpen
+                              ? `${workDays[5].from} - ${workDays[5].to}`
+                              : 'Зачинено'}
+                          </p>
+                        </li>
+                        <li>
+                          <b>Вс:</b>
+                          <p>
+                            {workDays[6].isOpen
+                              ? `${workDays[6].from} - ${workDays[6].to}`
+                              : 'Зачинено'}
+                          </p>
+                        </li>
+                      </ul>
+                    )}
+                    {isModalOpen && (
+                      <div
+                        className="modal-backdrop"
+                        onClick={handleClickOutside}
+                      />
+                    )}
+                  </ModalContent>
+                </div>
+              )}
+            </InfoBox>
           </li>
 
           <li>
-            <a href="">
+            <InfoBox>
               <b>Address</b>
-              <p>{address}</p>
-            </a>
+              <a href="">
+                <p>{address}</p>
+              </a>
+            </InfoBox>
           </li>
 
           <li>
-            <a href="">
+            <InfoBox>
               <b>Email</b>
-              <p>{email}</p>
-            </a>
+              <a href="">
+                <p>{email}</p>
+              </a>
+            </InfoBox>
           </li>
 
           <li>
-            <a href="">
+            <InfoBox>
               <b>Phone</b>
-              <p>{phone}</p>
-            </a>
+              <a href="">
+                <p>{phone}</p>
+              </a>
+            </InfoBox>
           </li>
         </ul>
       </ContentBox>
--- src/components/OurFriendsInfo/OurFriendsInfo.styled.js
+++ src/components/OurFriendsInfo/OurFriendsInfo.styled.js
@@ -18,4 +18,50 @@ a{
     text-align: left;
 }
 
+li:hover, a:hover, p:hover{
+  color: rgba(84, 173, 255, 1);
+}
   `
+
+
+export const InfoBox = styled.div`
+
+:hover{
+  color: rgba(84, 173, 255, 1);
+}
+
+li{
+  display: flex;
+  align-items: flex-start;
+  p{
+    text-align: right;
+  }
+}
+
+
+  `
+
+
+
+export const ModalContent = styled.div`
+
+position: absolute;
+display: flex;
+padding: 12px;
+background-color: #fff;
+border: 1px solid rgba(84, 173, 255, 1);
+border-radius: 8px;
+text-align: justify;
+
+
+font-weight: 500;
+font-size: 12px;
+
+color: black;
+
+b{
+  margin-right: 12px;
+}
+
+
+`
